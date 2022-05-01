2fa238602afb3bfa316654846e90268f0df6f61a
Vitor Henrique <vitorhenrique908@gmail.com>
2022-05-01 13:45:57 -0300

    Remove the objectType entity

--- src/main/resources/db/migration/V10__insert_object_type.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO object_type
-(id, name)
-VALUES(1, 'Celular/Tablet');
-INSERT INTO object_type
-(id, name)
-VALUES(2, 'Online');
-INSERT INTO object_type
-(id, name)
-VALUES(3, 'Download');
