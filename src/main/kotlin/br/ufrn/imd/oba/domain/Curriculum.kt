8b05cfa42ccb9271b95aac624c508a05c81a0463
Vitor Henrique <vitorhenrique908@gmail.com>
2022-05-15 14:52:47 -0300

    Remove unnused imports

--- src/main/kotlin/br/ufrn/imd/oba/domain/Curriculum.kt
+++ src/main/kotlin/br/ufrn/imd/oba/domain/Curriculum.kt
@@ -7,8 +7,6 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.SequenceGenerator
 import javax.persistence.Table
-import org.hibernate.annotations.Cache
-import org.hibernate.annotations.CacheConcurrencyStrategy
 
 @Entity
 @Table(name = "curriculum")
